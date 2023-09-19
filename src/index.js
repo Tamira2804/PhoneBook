dc6bd2c041d2d51d12fb77442dbd1e051e7b3c39
Tamira2804 <tamira2804@gmail.com>
2023-09-19 12:33:56 +0300

    create BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import 'modern-normalize/modern-normalize.css';
 import './styles/base.scss';
@@ -9,7 +10,9 @@ import './styles/base.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
