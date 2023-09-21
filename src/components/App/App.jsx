3f9d95d97390104462d5603671fa6ec554468d6c
Tamira2804 <tamira2804@gmail.com>
2023-09-21 18:07:27 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,55 +1,54 @@
-// import { useDispatch } from 'react-redux';
-// import { useEffect, lazy } from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import { RestrictedRoute } from '../RestrictedRoute';
-// import { PrivateRoute } from '../PrivateRoute';
-// import Layout from '../Layout';
-// import authOperations from 'redux/auth/auth-operations';
-// import { useAuth } from '../../hooks';
+import { useDispatch } from 'react-redux';
+import { useEffect, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { RestrictedRoute } from '../RestrictedRoute';
+import { PrivateRoute } from '../PrivateRoute';
+import Layout from '../Layout';
+import authOperations from 'redux/auth/auth-operations';
+import { useAuth } from '../../hooks';
 
-// const HomePage = lazy(() => import('../../pages/HomePage'));
-// const RegisterPage = lazy(() => import('../../pages/RegisterPage'));
-// const LoginPage = lazy(() => import('../../pages/LoginPage'));
-// const Contacts = lazy(() => import('../../pages/Contacts'));
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const RegisterPage = lazy(() => import('../../pages/RegisterPage'));
+const LoginPage = lazy(() => import('../../pages/LoginPage'));
+const Contacts = lazy(() => import('../../pages/Contacts'));
 
 const App = () => {
-  // const dispatch = useDispatch();
-  // const { isRefreshing } = useAuth();
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
-  // useEffect(() => {
-  //   dispatch(authOperations.fetchCurrentUser());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
 
-  return <h2> App </h2>;
-  // !isRefreshing ? (
-  //   <b>Refreshing user...</b>
-  // ) : (
-  //   <Routes>
-  //     <Route path="/" element={<Layout />}>
-  //       <Route index element={<HomePage />} />
-  //       <Route
-  //         path="/register"
-  //         element={
-  //           <RestrictedRoute
-  //             redirectTo="/contacts"
-  //             component={<RegisterPage />}
-  //           />
-  //         }
-  //       />
-  //       <Route
-  //         path="/login"
-  //         element={
-  //           <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
-  //         }
-  //       />
-  //       <Route
-  //         path="/contacts"
-  //         element={
-  //           <PrivateRoute redirectTo="/login" component={<Contacts />} />
-  //         }
-  //       />
-  //     </Route>
-  //   </Routes>
-  // );
+  return !isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Contacts />} />
+          }
+        />
+      </Route>
+    </Routes>
+  );
 };
 export default App;
